a1387e05322808b2338ec0320dbd28be68947b91
orindee <wwdsdd@gmail.com>
2021-08-24 19:38:48 -0400

    it's finished!!!
    i need to be better about committing while i go along....will be better about that in the next assignment!

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function () {
+  let listedPlanets;
+  let listedPlanetsResponse = myFetch();
 
-window.addEventListener("load", function() {
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+    })
+    .then(function () {
+      let chosenPlanet = pickPlanet(listedPlanets);
+      addDestinationInfo(
+        document,
+        chosenPlanet.name,
+        chosenPlanet.diameter,
+        chosenPlanet.star,
+        chosenPlanet.distance,
+        chosenPlanet.moons,
+        chosenPlanet.image
+      );
+    });
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    pilotInput = document.querySelector("input[name=pilotName]");
+    pilotValue = pilotInput.value;
+    copilotInput = document.querySelector("input[name=copilotName]");
+    copilotValue = copilotInput.value;
+    fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    fuelLevelValue = fuelLevelInput.value;
+    cargoLevelInput = document.querySelector("input[name=cargoMass]");
+    cargoLevelValue = cargoLevelInput.value;
+
+    let list = document.getElementById("faultyItems");
+    formSubmission(
+      document,
+      list,
+      pilotValue,
+      copilotValue,
+      fuelLevelValue,
+      cargoLevelValue
+    );
+  });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,43 +1,104 @@
-// Write your helper functions here!
-require('isomorphic-fetch');
+require("isomorphic-fetch");
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+function addDestinationInfo(
+  document,
+  name,
+  diameter,
+  star,
+  distance,
+  moons,
+  imageUrl
+) {
+  const div = document.getElementById("missionTarget");
+  div.innerHTML = `
+  <h2>Mission Destination</h2>
+  <ol>
+   <li>Name: ${name}</li>
+   <li>Diameter: ${diameter}</li>
+   <li>Star: ${star}</li>
+   <li>Distance from Earth: ${distance}</li>
+   <li>Number of Moons: ${moons}</li>
+   </ol>
+  <img src="${imageUrl}"/>
+  `;
 }
 
 function validateInput(testInput) {
-   
+  if (testInput === "") {
+    return "Empty";
+  } else if (isNaN(testInput)) {
+    return "Not a number";
+  } else if (testInput.type === Number) {
+    return "Is a number";
+  }
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+function formSubmission(
+  document,
+  list,
+  pilotValue,
+  copilotValue,
+  fuelLevelValue,
+  cargoLevelValue
+) {
+  pilotInput = document.querySelector("input[name=pilotName]");
+  pilotValue = pilotInput.value;
+  copilotInput = document.querySelector("input[name=copilotName]");
+  copilotValue = copilotInput.value;
+  fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+  fuelLevelValue = fuelLevelInput.value;
+  cargoLevelInput = document.querySelector("input[name=cargoMass]");
+  cargoLevelValue = cargoLevelInput.value;
+  list = document.getElementById("faultyItems");
+
+  if (
+    validateInput(pilotValue) === "Empty" ||
+    validateInput(copilotValue) === "Empty" ||
+    validateInput(fuelLevelValue) === "Empty" ||
+    validateInput(cargoLevelValue) === "Empty"
+  ) {
+    alert("All fields are required!");
+  } else if (
+    validateInput(cargoLevelValue) === "Not a number" ||
+    validateInput(fuelLevelValue) === "Not a number"
+  ) {
+    alert("You must enter a valid numerical input for these fields.");
+  } else {
+    list.style.visibility = "visible";
+    pilotStatus.innerHTML = `Pilot ${pilotValue} is ready for launch.`;
+    copilotStatus.innerHTML = `Copilot ${copilotValue} is ready for launch`;
+  }
+
+  if (fuelLevelValue <= 9999) {
+    launchStatus.innerText = "Shuttle not ready for launch.";
+    launchStatus.style.color = "red";
+    fuelStatus.innerText = "Fuel status not high enough to launch.";
+  } else if (cargoLevelValue >= 9999) {
+    launchStatus.innerText = "Shuttle not ready for launch.";
+    launchStatus.style.color = "red";
+    cargoStatus.innerText = "Cargo weight too much to launch.";
+  } else {
+    launchStatus.innerText = "Good to go!";
+    launchStatus.style.color = "green";
+  }
 }
 
 async function myFetch() {
-    let planetsReturned;
-
-    planetsReturned = await fetch().then( function(response) {
-        });
-
-    return planetsReturned;
+  const planetsReturned = await fetch(
+    "https://handlers.education.launchcode.org/static/planets.json"
+  ).then(function (response) {
+    return response.json();
+  });
+  return planetsReturned;
 }
 
 function pickPlanet(planets) {
+  let randomIndex = (Math.floor(Math.random()*planets.length));
+  return planets[randomIndex];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
